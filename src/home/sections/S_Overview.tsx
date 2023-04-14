be78b53c4eda6e46f1054d94057a078f2c327fad
Jasmail Singh Duck <jasmailduck@icloud.com>
2023-04-14 13:47:28 -0600

    Added More Content to Home Overview Screen

--- src/home/sections/S_Overview.tsx
+++ src/home/sections/S_Overview.tsx
@@ -1,3 +1,5 @@
+import { ArrowRightIcon } from "@heroicons/react/24/solid";
+
 const S_Overview = () => {
   return (
     <section id="S_OverviewWrapper" className=" flex justify-center">
@@ -6,16 +8,34 @@ const S_Overview = () => {
         className=" max-w-screen-xl w-full border h-screen flex px-5"
       >
         <div className="border flex-1 flex items-start">
-          <div className="border h-96 mt-32">
+          <div className="border h-96 mt-32 w-full">
             <div>
-              <div className=" text-7xl font-extrabold">
-                <span>Fullstack Developer</span>Some really brave big text the
-                should fit 3 lines
+              <div className=" text-6xl font-extrabold flex flex-col">
+                <span className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-green-400 mb-1">
+                  Fullstack Developer
+                </span>
+                <span className="mb-1">Data Analyst</span>
+                <span className="mb-1">BCIS</span>
               </div>
-              <h2 className="text-xl mt-5">
-                Some very small text the should describe what i do and other
-                stugg
+              <h2 className="text-xl my-5  text-neutral-500">
+                Hi, I'm Jasmail. Driven by an unwavering passion for coding, I
+                am constantly seeking to expand my knowledge and skills beyond
+                the boundaries of what is currently known.
               </h2>
+
+              <div className=" flex w-full h-12 font-bold">
+                <div className=" relative w-40">
+                  <div className="h-12 w-36 bg-gradient-to-r from-cyan-400 to-green-400 absolute top-2 left-2 rounded-md"></div>
+                  <button className=" bg-black absolute h-12 w-36 text-white rounded-md">
+                    Contact
+                  </button>
+                </div>
+
+                <div className="ml-5 text-xl self-center flex  ">
+                  <h1>Download Resume</h1>
+                  <ArrowRightIcon className="ml-3 w-6" />
+                </div>
+              </div>
             </div>
           </div>
         </div>
