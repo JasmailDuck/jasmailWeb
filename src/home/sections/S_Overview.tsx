ce6bb8d0ce35c70d62cf0455bb9834049adf64e4
Jasmail Singh Duck <jasmailduck@icloud.com>
2023-04-14 13:12:21 -0600

    added pretty

--- src/home/sections/S_Overview.tsx
+++ src/home/sections/S_Overview.tsx
@@ -1,29 +1,29 @@
-
 const S_Overview = () => {
   return (
     <section id="S_OverviewWrapper" className=" flex justify-center">
-
-        <div id="S_OverviewContainer" className=" max-w-screen-xl w-full border h-screen flex px-5">
-
-            <div className="border flex-1 flex items-start">
-
-                <div className="border h-96 mt-32">
-                    <div>
-                        <h1 className=" text-7xl">Some really brave big text the should fit 3 lines</h1>
-                        <h2 className="text-xl mt-5">Some very small text the should describe what i do and other stugg</h2>
-                    </div>
-                </div>
-
+      <div
+        id="S_OverviewContainer"
+        className=" max-w-screen-xl w-full border h-screen flex px-5"
+      >
+        <div className="border flex-1 flex items-start">
+          <div className="border h-96 mt-32">
+            <div>
+              <div className=" text-7xl font-extrabold">
+                <span>Fullstack Developer</span>Some really brave big text the
+                should fit 3 lines
+              </div>
+              <h2 className="text-xl mt-5">
+                Some very small text the should describe what i do and other
+                stugg
+              </h2>
             </div>
-
-            <div className="border flex-1">
-
-            </div>
-
+          </div>
         </div>
 
+        <div className="border flex-1 "></div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default S_Overview
+export default S_Overview;
