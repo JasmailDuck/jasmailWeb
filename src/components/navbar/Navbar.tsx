7ed7adcf600a0f5700b0c5cbdb167521948471ec
Jasmail Singh Duck <jasmailduck@icloud.com>
2023-05-05 13:21:20 -0600

    Added back screens

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { AnimatePresence, motion as m } from "framer-motion";
+import { AnimatePresence, delay, motion as m } from "framer-motion";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import GithubIcon from "../../assets/icons/I_Github.svg";
 import LinkedInIcon from "../../assets/icons/I_LinkedIn.svg";
@@ -7,28 +7,23 @@ import TwitterIcon from "../../assets/icons/I_Twitter.svg";
 import GmailIcon from "../../assets/icons/I_Gmail.svg";
 
 const Navbar = () => {
-  // let aboutSection: HTMLElement | null;
-  // let servicesSection: HTMLElement | null;
-  // let projectSection: HTMLElement | null;
+  let aboutSection: HTMLElement | null;
+  let servicesSection: HTMLElement | null;
+  let projectSection: HTMLElement | null;
 
-  // const handleScrollToOnClick = (element: HTMLElement | null) => {
-  //   if (element) element.scrollIntoView({ behavior: "smooth" });
-  // };
+  const handleScrollToOnClick = (element: HTMLElement | null) => {
+    if (element) element.scrollIntoView({ behavior: "smooth" });
+  };
 
-  // useEffect(() => {
-  //   aboutSection = document.getElementById("S_AboutWrapper");
-  //   servicesSection = document.getElementById("S_ServiceWrapper");
-  //   projectSection = document.getElementById("S_ProjectWrapper");
-  // });
+  useEffect(() => {
+    aboutSection = document.getElementById("S_AboutWrapper");
+    servicesSection = document.getElementById("S_ServiceWrapper");
+    projectSection = document.getElementById("S_ProjectWrapper");
+  });
 
-  const [showMenu, setShowMenu] = useState<String>("");
-
-  return (
-    <nav id="navbarwrapper" className=" flex justify-center flex-col">
-      <div
-        id="navbarcontainer"
-        className=" lg:max-w-screen-xl w-full p-2 text-lg h-20 flex flex-col"
-      >
+  const mobileNavigationLayout = () => {
+    return (
+      <>
         <m.div
           initial={{ display: "none", opacity: 0 }}
           animate={
@@ -39,7 +34,7 @@ const Navbar = () => {
           onClick={() => {
             setShowMenu("");
           }}
-          className=" h-20 rounded-lg justify-end px-2 mt-4"
+          className=" h-20 rounded-lg justify-end px-2 mt-4 lg:hidden"
         >
           <XMarkIcon className=" w-10 text-white bg-neutral-700 p-1 rounded-md" />
         </m.div>
@@ -49,19 +44,65 @@ const Navbar = () => {
           onClick={() => {
             setShowMenu("displayMenu");
           }}
-          className=" bg-neutral-800 w-full h-20 rounded-lg flex items-center px-2"
+          className=" bg-neutral-800 w-full h-20 rounded-lg flex items-center px-2 lg:hidden"
         >
-          <h1 className=" flex-1 text-white  ">Jasmail Duck</h1>
+          <h1 className=" flex-1 text-white text-xl  ">Jasmail Duck</h1>
           <Bars3Icon className=" w-10 text-white bg-neutral-700 p-1 rounded-md" />
         </m.div>
+      </>
+    );
+  };
+
+  const desktopNavigationLayout = () => {
+    return (
+      <m.div className=" bg-neutral-800 w-full h-20 rounded-lg lg:flex items-center px-5 hidden">
+        <h1 className=" flex-1 text-white text-xl ">Jasmail Duck</h1>
+        <div className=" flex">
+          <m.div
+            onClick={() => handleScrollToOnClick(aboutSection)}
+            className=" w-full text-white text-xl p-2 flex items-center uppercase hover:cursor-pointer"
+          >
+            About
+          </m.div>
+          <m.div
+            onClick={() => handleScrollToOnClick(servicesSection)}
+            className=" w-full text-white text-xl p-2 flex items-center uppercase hover:cursor-pointer"
+          >
+            Services
+          </m.div>
+          <m.div
+            onClick={() => handleScrollToOnClick(projectSection)}
+            className=" w-full text-white text-xl p-2 flex items-center uppercase hover:cursor-pointer"
+          >
+            Projects
+          </m.div>
+        </div>
+      </m.div>
+    );
+  };
+
+  const [showMenu, setShowMenu] = useState<String>("");
+
+  return (
+    <nav
+      id="navbarwrapper"
+      className=" flex justify-center items-center w-full  flex-col"
+    >
+      <div
+        id="navbarcontainer"
+        className=" lg:max-w-screen-xl w-full p-2 text-lg h-20 flex flex-col"
+      >
+        {mobileNavigationLayout()}
+        {desktopNavigationLayout()}
       </div>
 
       <AnimatePresence>
         {showMenu == "displayMenu" && (
-          <m.div className=" h-96 w-full p-2 ">
+          <m.div className="w-full p-2 flex justify-center items-center flex-col ">
             <m.div
               layoutId="displayMenu"
-              className=" bg-neutral-800 rounded-lg tracking-wider flex flex-col p-2 mb-5"
+              exit={{ transition: { delay: 0.5 } }}
+              className=" bg-neutral-800 rounded-lg tracking-wider flex flex-col p-2 mb-5 lg:max-w-screen-xl w-full"
             >
               <m.div className=" w-full text-white text-xl p-2 flex items-center mb-2 uppercase">
                 About
@@ -81,6 +122,11 @@ const Navbar = () => {
                 translateY: 0,
                 transition: { delay: 0.2, ease: "easeInOut" },
               }}
+              exit={{
+                opacity: 0,
+                translateY: -10,
+                transition: { ease: "easeInOut" },
+              }}
               className=" font-bold text-3xl text-white tracking-wider"
             >
               Learn More About Me
@@ -91,6 +137,11 @@ const Navbar = () => {
                 opacity: 1,
                 translateY: 0,
                 transition: { delay: 0.3, ease: "easeInOut" },
+              }}
+              exit={{
+                opacity: 0,
+                translateY: -10,
+                transition: { ease: "easeInOut" },
               }}
               className=" text-lg text-neutral-400 tracking-wider mb-3"
             >
@@ -104,6 +155,11 @@ const Navbar = () => {
                   translateY: 0,
                   transition: { delay: 0.4, ease: "easeInOut" },
                 }}
+                exit={{
+                  opacity: 0,
+                  translateY: -10,
+                  transition: { ease: "easeInOut" },
+                }}
                 className=" bg-purple-900 w-14 h-14 flex justify-center items-center rounded-xl"
               >
                 <img src={GithubIcon} alt="" className="w-1/2" />
@@ -114,6 +170,11 @@ const Navbar = () => {
                   opacity: 1,
                   translateY: 0,
                   transition: { delay: 0.4, ease: "easeInOut" },
+                }}
+                exit={{
+                  opacity: 0,
+                  translateY: -10,
+                  transition: { ease: "easeInOut" },
                 }}
                 className=" bg-[#0a66c2] w-14 h-14 flex justify-center items-center rounded-xl"
               >
@@ -126,6 +187,11 @@ const Navbar = () => {
                   translateY: 0,
                   transition: { delay: 0.4, ease: "easeInOut" },
                 }}
+                exit={{
+                  opacity: 0,
+                  translateY: -10,
+                  transition: { ease: "easeInOut" },
+                }}
                 className=" bg-[#1da1f2] w-14 h-14 flex justify-center items-center rounded-xl"
               >
                 <img src={TwitterIcon} alt="" className="w-1/2" />
@@ -136,6 +202,11 @@ const Navbar = () => {
                   opacity: 1,
                   translateY: 0,
                   transition: { delay: 0.4, ease: "easeInOut" },
+                }}
+                exit={{
+                  opacity: 0,
+                  translateY: -10,
+                  transition: { ease: "easeInOut" },
                 }}
                 className=" bg-[#ea4335] w-14 h-14 flex justify-center items-center rounded-xl"
               >
