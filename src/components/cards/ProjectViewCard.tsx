9e487b908ff4d04b62d5cb6799d254c4742357fb
Jasmail Singh Duck <jasmailduck@icloud.com>
2023-06-13 14:50:15 -0600

    Added new expansion function

--- src/components/cards/ProjectViewCard.tsx
+++ src/components/cards/ProjectViewCard.tsx
@@ -1,19 +1,31 @@
-import { ArrowRightCircleIcon } from "@heroicons/react/24/outline";
 import AttributeTextWithBorder from "../text/AttributeTextWithBorder";
-import { ArrowDownCircleIcon } from "@heroicons/react/24/outline";
+import {
+  ArrowDownCircleIcon,
+  ArrowUpCircleIcon,
+} from "@heroicons/react/24/outline";
+import { useState } from "react";
 
 const ProjectViewCard = () => {
+  const [expanded, setExpanded] = useState(true);
+
   return (
-    <div className=" border border-neutral-700 h-64 bg-neutral-800/40 rounded-lg flex flex-col p-3">
+    <div className=" border border-neutral-700  bg-neutral-800/40 rounded-lg flex flex-col p-3">
       <div className=" flex  mb-2">
         <div className=" flex gap-2 flex-1">
           <AttributeTextWithBorder title="Data" />
           <AttributeTextWithBorder title="Full-Stack" />
         </div>
 
-        <div className=" flex gap-2">
+        <div
+          className=" flex gap-2 hover:cursor-pointer"
+          onClick={() => setExpanded(!expanded)}
+        >
           <h1 className=" text-neutral-500">More Info </h1>
-          <ArrowDownCircleIcon className=" w-5 text-neutral-500" />
+          {expanded ? (
+            <ArrowDownCircleIcon className=" w-5 text-neutral-500" />
+          ) : (
+            <ArrowUpCircleIcon className=" w-5 text-neutral-500" />
+          )}
         </div>
       </div>
 
@@ -41,6 +53,16 @@ const ProjectViewCard = () => {
           className=" w-1/3 h-40 rounded-lg object-cover "
         />
       </div>
+
+      {expanded && (
+        <div className=" flex flex-col gap-1 mt-2">
+          <h1 className=" text-neutral-200 text-xs">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod magni
+            natus distinctio minus consequuntur atque dolorum adipisci, quaerat
+            laborum itaque!
+          </h1>
+        </div>
+      )}
     </div>
   );
 };
